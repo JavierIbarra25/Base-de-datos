5fbcb9babe2306aef8a504bbfead9d3b755904ca
Javier_Ibarra <javiermaria.s463101@cesurformacion.com>
2024-12-04 01:07:22 +0100

    Add files via upload

---
+++ Aviones.sql
@@ -0,0 +1,160 @@
+DROP DATABASE IF EXISTS Aviones;
+CREATE DATABASE IF NOT EXISTS Aviones CHARSET utf8mb4 COLLATE utf8mb4_spanish2_ci;
+
+USE Aviones;
+
+CREATE TABLE Tripulacion (
+ NIF VARCHAR (12) NOT NULL,
+ Nombre VARCHAR (25) NOT NULL,
+ Apellido VARCHAR (25) NOT NULL,
+ Piloto VARCHAR(2) NOT NULL,
+ CoPiloto VARCHAR(2) NOT NULL,
+ JefeDeCabina VARCHAR(2) NOT NULL,
+ Azafato_a VARCHAR(2) NOT NULL,
+ FechaNacimiento DATE,
+ CAT1 VARCHAR(2),
+ CAT2 VARCHAR(2),
+ CAT3 VARCHAR(2),
+ Calle VARCHAR (52) NOT NULL,
+ Numero NUMERIC (5) NOT NULL,
+ CP NUMERIC (5) NOT NULL,
+
+ CONSTRAINT PK_NIF PRIMARY KEY (NIF),
+ CONSTRAINT CH_Piloto CHECK (Piloto = "Si" OR Piloto = "No"),
+ CONSTRAINT CH_CoPiloto CHECK (CoPiloto = "Si" OR CoPiloto = "No"),
+ CONSTRAINT CH_JefeDeCabina CHECK (JefeDeCabina = "Si" OR JefeDeCabina = "No"),
+ CONSTRAINT CH_Azafato_a CHECK (Azafato_a = "Si" OR Azafato_a = "No"),
+ CONSTRAINT CH_CAT1 CHECK (CAT1 = "Si"),
+ CONSTRAINT CH_CAT2 CHECK (CAT2 = "Si"),
+ CONSTRAINT CH_CAT3 CHECK (CAT3 = "Si")
+ )
+ ENGINE = InnoDB;
+ 
+ CREATE TABLE Telefonos (
+ NIF_Usuarios VARCHAR (14) NOT NULL,
+ Telefono VARCHAR (14) NOT NULL,
+ 
+ CONSTRAINT PK_Telefono PRIMARY KEY (Telefono),
+ CONSTRAINT NIF_Usuarios FOREIGN KEY (NIF_Usuarios) 
+	REFERENCES Tripulacion (NIF)
+	ON DELETE CASCADE
+  )
+  ENGINE = InnoDB;
+  
+CREATE TABLE Tripulantes (
+ NIF1 VARCHAR (14) NOT NULL,
+ NIF2 VARCHAR (14) NOT NULL,
+ Armonia VARCHAR (14) NOT NULL,
+ 
+ CONSTRAINT CH_Armonia CHECK (Armonia = "Si" OR Armonia = "No"),
+ CONSTRAINT NIF1 FOREIGN KEY (NIF1) 
+	REFERENCES Tripulacion (NIF)
+	ON DELETE CASCADE,
+ CONSTRAINT NIF2 FOREIGN KEY (NIF2) 
+	REFERENCES Tripulacion (NIF)
+	ON DELETE CASCADE
+  )
+  ENGINE = InnoDB;
+  
+CREATE TABLE Modelos (
+ Codigo_Modelos VARCHAR (25) NOT NULL,
+ PrimeraCLase NUMERIC(5) NOT NULl,
+ ClaseEconomica NUMERIC(5) NOT NULl,
+ Numero NUMERIC(5) NOT NULl,
+ Fuel NUMERIC(25) NOT NULl,
+ 
+ CONSTRAINT PK_Codigo_Modelos PRIMARY KEY (Codigo_Modelos),
+ CONSTRAINT CH_PrimeraCLase CHECK (PrimeraCLase >= 0),
+ CONSTRAINT CH_ClaseEconomica CHECK (ClaseEconomica >= 0)
+  )
+  ENGINE = InnoDB;
+  
+CREATE TABLE Aviones (
+ CodigoModelos VARCHAR (25) NOT NULL,
+ Matricula VARCHAR (25) NOT NULL,
+ Nombre VARCHAR (14),
+ 
+ CONSTRAINT PK_Matricula PRIMARY KEY (Matricula),
+ CONSTRAINT Fk_CodigoModelos FOREIGN KEY (CodigoModelos) 
+	REFERENCES Modelos (Codigo_Modelos)
+	ON DELETE CASCADE
+  )
+  ENGINE = InnoDB;
+  
+CREATE TABLE Vuelos (
+ Codigo_Vuelos VARCHAR (25) NOT NULL,
+ MatriculaAviones VARCHAR (25) NOT NULL,
+ AeropuertoEntrada VARCHAR (25) NOT NULL,
+ AeropuertoSalida  VARCHAR (25) NOT NULL,
+ FuelMin  NUMERIC (25) NOT NULL,
+ FechaEntrada DATE NOT NULL,
+ FechaSalida DATE NOT NULL,
+ 
+ CONSTRAINT PK_Codigo_Vuelos PRIMARY KEY (Codigo_Vuelos),
+ CONSTRAINT FK_MatriculaAviones FOREIGN KEY (MatriculaAviones) 
+	REFERENCES Aviones (Matricula)
+	ON DELETE CASCADE
+  )
+  ENGINE = InnoDB;
+ 
+ CREATE TABLE Licencias (
+ NIF_Tripulantes VARCHAR (14) NOT NULL,
+ Licencia VARCHAR (25),
+ Fecha_Inicio DATE,
+ Fecha_Caducidad DATE,
+ 
+ CONSTRAINT PK_Fecha_Caducidad PRIMARY KEY (Fecha_Caducidad),
+ CONSTRAINT Fk_NIF_Tripulantes FOREIGN KEY (NIF_Tripulantes) 
+	REFERENCES Tripulacion (NIF)
+	ON DELETE CASCADE
+  )
+  ENGINE = InnoDB;
+  
+CREATE TABLE Asignados (
+ NIF_Asignados VARCHAR (14) NOT NULL,
+ CodigoVuelos VARCHAR (25) NOT NULL,
+ FechaCaducidad DATE NOT NULL,
+ 
+ CONSTRAINT NIF_Asignados FOREIGN KEY (NIF_Asignados) 
+	REFERENCES Tripulacion (NIF)
+	ON DELETE CASCADE,
+ CONSTRAINT CodigoVuelos FOREIGN KEY (CodigoVuelos) 
+	REFERENCES Vuelos (Codigo_Vuelos)
+	ON DELETE CASCADE,
+ CONSTRAINT FechaCaducidad FOREIGN KEY (FechaCaducidad) 
+	REFERENCES Licencias (Fecha_Caducidad)
+	ON DELETE CASCADE   
+  )
+  ENGINE = InnoDB;
+  
+CREATE TABLE APEntrada (
+ Codigo_IATA VARCHAR (25) NOT NULL,
+ Pais VARCHAR (25) NOT NULL,
+ GPS VARCHAR (25) NOT NULL,
+ FranjaHoraria DATE NOT NULL,
+ CAT1 VARCHAR(2),
+ CAT2 VARCHAR(2),
+ CAT3 VARCHAR(2),
+
+ CONSTRAINT PK1_Codigo_IATA PRIMARY KEY (Codigo_IATA),
+ CONSTRAINT CH1_CAT1 CHECK (CAT1 = "Si"),
+ CONSTRAINT CH2_CAT2 CHECK (CAT2 = "Si"),
+ CONSTRAINT CH3_CAT3 CHECK (CAT3 = "Si")
+ )
+ ENGINE = InnoDB;
+ 
+CREATE TABLE APSalida (
+ Codigo_IATA VARCHAR (25) NOT NULL,
+ Pais VARCHAR (25) NOT NULL,
+ GPS VARCHAR (25) NOT NULL,
+ FranjaHoraria DATE NOT NULL,
+ CAT1 VARCHAR(2),
+ CAT2 VARCHAR(2),
+ CAT3 VARCHAR(2),
+
+ CONSTRAINT PK2_Codigo_IATA PRIMARY KEY (Codigo_IATA),
+ CONSTRAINT CH4_CAT1 CHECK (CAT1 = "Si"),
+ CONSTRAINT CH5_CAT2 CHECK (CAT2 = "Si"),
+ CONSTRAINT CH6_CAT3 CHECK (CAT3 = "Si")
+ )
+ ENGINE = InnoDB;
